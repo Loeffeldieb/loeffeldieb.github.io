0a0ce4ffbb30fb17a57bdbf0498ecc97ed5ff566
Loeffeldieb <jollegaming@gmx.de>
2024-05-23 11:46:31 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,99 +1,69 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { XRButton } from 'three/addons/webxr/XRButton.js';
-//Für Hand Input
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
 import { XRHandModelFactory } from 'three/addons/webxr/XRHandModelFactory.js';
 
 
-/*
-                        Three.js Setup   Initialisierung
-*/
+/**************************************************************************************************************************
+                                            Three.js Setup   Initialisierung
+***************************************************************************************************************************/
 
-//Variabllen für die Hand Erkennung
-let hand1, hand2;
-let controller1, controller2;
-let controllerGrip1, controllerGrip2;
-let controls;
-
-//Initialisiere Szene
-const scene = new THREE.Scene();
-//Kamera (FoV, AR, Near, Far Render Distance)
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//Initialisiere Renderer und füge dem DOM hinzu
 const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.xr.enabled = true;
 
-//Camera Adjustment
+const scene = new THREE.Scene();
+//Kamera (FoV, AR, Near, Far Render Distance)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 1.6, 10);
 
 //Lichtquelle
 const light = new THREE.AmbientLight( 0xFFFFFF, 3.0 ); // soft white light
 scene.add( light );
 
-//Initialisiere WebXR & ARButton
+//Starte XR mit Button
 const sessionInit = {
   optionalFeatures: [ 'hand-tracking' ]
-  //requiredFeatures: ["hand-tracking"],
 };
-
-renderer.xr.enabled = true;
 document.body.appendChild(XRButton.createButton(renderer, sessionInit));
 document.body.appendChild( renderer.domElement );
 
-//Orbit Controls
-controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 1.6, 0 );
-controls.update();
+//Orbit Controls (Bewegen der Szene mit Maus)
+let orbitControls;
+orbitControls = new OrbitControls( camera, renderer.domElement );
+orbitControls.target.set( 0, 1.6, 0 );
+orbitControls.update();
 
+//Variablen für die Hand Erkennung und Controller
+let controller, hand, controllerGrip;
 
-
-
-// controllers
-
-controller1 = renderer.xr.getController( 0 );
-scene.add( controller1 );
-
-controller2 = renderer.xr.getController( 1 );
-scene.add( controller2 );
-
+//Factories zum zeichnen der Controller
 const controllerModelFactory = new XRControllerModelFactory();
 const handModelFactory = new XRHandModelFactory();
 
-// Hand 1
-controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-scene.add( controllerGrip1 );
+//Standard Controller
+controller = renderer.xr.getController( 0 );
+scene.add( controller );
 
-hand1 = renderer.xr.getHand( 0 );
-hand1.add( handModelFactory.createHandModel( hand1 ) );
+//Grip Controller
+controllerGrip = renderer.xr.getControllerGrip( 0 );
+controllerGrip.add( controllerModelFactory.createControllerModel( controllerGrip ) );
+scene.add( controllerGrip );
 
-scene.add( hand1 );
+//Hand Controller
+hand = renderer.xr.getHand( 0 );
+hand.add( handModelFactory.createHandModel( hand ) );
+scene.add( hand );
 
-// Hand 2
-controllerGrip2 = renderer.xr.getControllerGrip( 1 );
-controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
-scene.add( controllerGrip2 );
-
-hand2 = renderer.xr.getHand( 1 );
-hand2.add( handModelFactory.createHandModel( hand2 ) );
-scene.add( hand2 );
-
-//
-
+//Zeichnet Linie ausgehend vom Controller
 const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
-
 const line = new THREE.Line( geometry );
 line.name = 'line';
 line.scale.z = 5;
-
-controller1.add( line.clone() );
-controller2.add( line.clone() );
-
-
-
+controller.add( line.clone() );
 
 /*
                         PickHelper Class importiert von Three.js examples
@@ -130,21 +100,33 @@ class PickHelper {
 const pickHelper = new PickHelper();
 */
 
-/*
-                        Model einladen durhc erstellen von Promise
-*/
-function load3DModel(url){
-    return new Promise(resolve => {
-        new GLTFLoader().load(url,resolve);
-    });
+
+
+/**************************************************************************************************************************
+                                            Interaction
+***************************************************************************************************************************/
+
+//Zeichnet Frame
+function render(){
+  renderer.render( scene, camera);
 };
 
-let model1;
+//Animation Loop
+function animate(time) {
+  time = Math.floor(time*0.01); 
+  //Model Animation
+  model1.position.z = -5;
 
-let p = load3DModel('Duck.gltf').then(result => {model1 = result.scene});
+  // 0, 0 is the center of the view in normalized coordinates.
+  /*pickHelper.pick({x: 0, y: 0}, scene, camera, time);*/
+
+  render();
+}
+
+let model1;
+let p = loadGLTF('Duck.gltf').then(result => {model1 = result.scene});
 
 Promise.all([p]).then( () => {
-
     // Bearbeite Model
     model1.position.set(0,0,0);
     scene.add(model1);
@@ -153,9 +135,18 @@ Promise.all([p]).then( () => {
     renderer.setAnimationLoop(animate);
 });
 
+function loadGLTF(url){
+  return new Promise(resolve => {
+      new GLTFLoader().load(url,resolve);
+  });
+};
+
+
+//Select Event 
+controller.addEventListener( 'select', onSelect );
 
 function onSelect(){
-    load3DModel('Duck.gltf').then(result => {
+    loadGLTF('Duck.gltf').then(result => {
       result.scene.position.set(0,0,-5.0).applyMatrix4( controller.matrixWorld );
       result.scene.quaternion.setFromRotationMatrix( controller.matrixWorld );
       result.scene.scale.set(0.2, 0.2, 0.2);
@@ -164,40 +155,14 @@ function onSelect(){
     });
 };
 
-//let controller = renderer.xr.getController( 0 );
-controller1.addEventListener( 'select', onSelect );
-//scene.add( controller );
 
-/*
-                        Animation Loop
-*/
+/**************************************************************************************************************************
+                                            Window Resize Event
+***************************************************************************************************************************/
 
-
-function animate(time) {
-    time = Math.floor(time*0.01); 
-    //Model Animation
-    model1.position.z = -5;
-
-    // 0, 0 is the center of the view in normalized coordinates.
-    /*pickHelper.pick({x: 0, y: 0}, scene, camera, time);*/
-
-    //Rendert einen Frame
-    render();
-}
-
-function render(){
-    renderer.render( scene, camera);
-};
-
-
-
-
-/*
-                        Reset On Resize
-*/
 addEventListener("resize", (event) => {
+    //Funktioniert nicht mit XR?
     if(renderer.xr.enabled === false){
-        // Update camera
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix()
         renderer.setSize( window.innerWidth, window.innerHeight );
