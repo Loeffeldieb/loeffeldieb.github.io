77868208e61c092809daaf1c8b17dd8eb91aeb19
Loeffeldieb <jollegaming@gmx.de>
2024-06-11 16:31:15 +0200

    Update main3.js

--- main3.js
+++ main3.js
@@ -3,26 +3,27 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 //XR Spezifisch
-import { XRButton } from 'three/addons/webxr/XRButton.js';
-import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
-import { XRHandModelFactory } from 'three/addons/webxr/XRHandModelFactory.js';
+//import { XRButton } from 'three/addons/webxr/XRButton.js';
+
 
 /**************************************************************************************************************************
                                             Three.js Setup   Initialisierung
 ***************************************************************************************************************************/
 
-const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );  //  <-----
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: false } );  //  <-----
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.xr.enabled = true;                                                    //  <-----
+renderer.xr.enabled = false;                                                    //  <-----
 
 const scene = new THREE.Scene();
 
 //Kamera (FoV, AR, Near, Far Render Distance)
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0, 1.6, 10);
+camera.position.set(0, 10, 0);
+camera.lookAt(0,0,0);
+camera.updateProjectionMatrix();
 
-const light = new THREE.HemisphereLight(new THREE.Color(0xff0000), new THREE.Color(0x0000ff), 1);
+const light = new THREE.HemisphereLight(new THREE.Color(0xffffff), new THREE.Color(0xffffff), 1);
 scene.add( light );
 
 document.body.appendChild( renderer.domElement );
@@ -37,7 +38,8 @@ orbitControls = new OrbitControls( camera, renderer.domElement );
 orbitControls.target.set( 0, 1.6, 0 );
 orbitControls.update();
 
-//Starte XR mit Button
+/*
+Starte XR mit Button
 const sessionInit = {
     optionalFeatures: [ 
         'hand-tracking',
@@ -45,27 +47,17 @@ const sessionInit = {
         ]
 };
 
-document.body.appendChild(XRButton.createButton(renderer, sessionInit));
-
-
-
+document.body.appendChild(XRButton.createButton(renderer, sessionInit));           // <-----
 
 
 //Variablen für die Hand Erkennung und Controller
-let controller, hand, controllerGrip;
+let controller;
 
-//Factories zum zeichnen der Controller
-const controllerModelFactory = new XRControllerModelFactory();
-const handModelFactory = new XRHandModelFactory();
 
 //Standard Controller
 controller = renderer.xr.getController( 0 );
 scene.add( controller );
-
-//Hand Controller
-hand = renderer.xr.getHand( 0 );
-hand.add( handModelFactory.createHandModel( hand ) );
-scene.add( hand );
+*/
 
 /**************************************************************************************************************************
                                             Starte Renderer nach Vorbereitungen wenn nötig
@@ -104,15 +96,26 @@ function animate(timestamp, frame) {
     //Animationen
     sphere.material.color = changeColor(timestamp);
 
-    for(let i = 0; i < scene.enten.length; i++){
-        let obj = scene.enten[i];
-        obj.applyForce(gravity);
-        obj.updateSpeed();
-        if(obj.updateAge){
-            obj.updateAge(timestamp);
-            if(obj.age >= 20){
-            scene.remove(obj);
-            scene.enten.splice(i,1)};
+    let zeit = Math.floor(timestamp*0.1);
+    let theta = Math.PI / 180 * (360 * Math.random());
+    if(zeit%10 == 0){
+        scene.movers.push(new Mover(new THREE.Vector3( 7*Math.sin(theta), 0, 7*Math.cos(theta) )));
+    };
+
+    //Loop durch Mover
+    for(let i=0; i<scene.movers.length; i++){
+        const foo = new THREE.Vector3().subVectors(sphere.position, scene.movers[i].body.position);
+        foo.normalize()
+        foo.multiplyScalar(scene.movers[i].maxSpeed);
+        let target = scene.movers[i].seek(sphere.position);
+        scene.movers[i].body.lookAt(target);
+        scene.movers[i].applyForce(target);
+        scene.movers[i].updatePosition();
+
+        if(scene.movers[i].isAlive == false){
+            scene.remove(scene.movers[i].body);
+            scene.movers.splice(i,1);
+            console.log(scene.children.length, scene.movers.length);
         };
     };
 
@@ -133,7 +136,53 @@ const sphere = new THREE.Mesh(
 );
 
 scene.add(sphere);
-sphere.position.set(0,0,-5);
+sphere.position.set(0,0,0);
+sphere.name = 'attractor';
+
+//Klasse für Verfolger
+class Mover{
+
+    constructor(p){
+        this.velocity = new THREE.Vector3(0,0,0);
+        this.acceleration = new THREE.Vector3(0,0,0);
+        this.mass = Math.random();
+        this.force = 1.0;
+        this.maxSpeed = 0.01;
+        this.body = this.createMesh();
+        this.isAlive = true;
+        this.body.position.set(p.x,p.y,p.z);
+        scene.add(this.body); // Muss Raus später
+    };
+
+    createMesh(){
+        let mesh = new THREE.Mesh(
+            new THREE.OctahedronGeometry( this.mass ),
+            new THREE.MeshPhongMaterial({
+                color: new THREE.Color(Math.random(), Math.random(), Math.random()),
+                side: THREE.FrontSide,
+            })
+        );
+        return mesh;
+    };
+
+    seek(target){
+        let desiredVelocity = new THREE.Vector3().subVectors(target, this.body.position);
+        if(desiredVelocity.length() < 0.5) this.isAlive = false;
+        desiredVelocity.multiplyScalar(this.maxSpeed);
+        let steering = new THREE.Vector3().subVectors(desiredVelocity, this.velocity);
+        return steering;
+    };
+
+    applyForce(source){
+        this.acceleration.add(source);
+    };
+
+    updatePosition(){
+        this.velocity.add(this.acceleration);
+        this.body.position.add(this.velocity);
+        this.acceleration.multiplyScalar(0);
+    };
+};
 
 /**************************************************************************************************************************
                                            Funktions
@@ -144,7 +193,6 @@ function changeColor(time){
     const c = (t%2 == 0)?new THREE.Color(0xFF8844):new THREE.Color(0x8844FF);
     return c;
 };
-
 
 
 function loadGLTF(url){
@@ -160,15 +208,16 @@ function loadGLTF(url){
 
 
 //Select Event 
-controller.addEventListener( 'select', onSelect );
+//controller.addEventListener( 'select', onSelect );
+
+const test = document.getElementsByTagName('canvas')[0];
+test.addEventListener('click', onSelect);
+
+scene.movers = [];
 
 function onSelect(){
-    loadGLTF('Duck.gltf').then(result => {
-      let gltf = result.scene;
-      initDuck(gltf);
-      scene.enten.push(result.scene);
-      scene.add(scene.enten[scene.enten.length-1]);
-    });
+    let foo = new Mover(new THREE.Vector3(0,0,0));
+    scene.movers.push(foo);
 };
 
 
@@ -184,62 +233,3 @@ addEventListener("resize", (event) => {
         renderer.setSize( window.innerWidth, window.innerHeight );
     };
   })
-
-
-
-
-scene.enten = [];
-const gravity = new THREE.Vector3(0,-0.005,0);
-
-function initDuck(obj){
-    obj.position.set(0,0,-Math.random()*5).applyMatrix4( controller.matrixWorld );
-    const theta = 360 * Math.random() * (Math.PI / 180);
-    obj.rotateY(theta);
-    const scaling = Math.random()*0.33;
-    obj.scale.set(scaling, scaling, scaling);
-
-    obj.children[0].children[0].material.color = new THREE.Color(Math.random(), Math.random(), Math.random());
-    obj.children[0].children[0].material.emissive = new THREE.Color(Math.random(), Math.random(), Math.random());
-
-    obj.acceleration = new THREE.Vector3(0,0,0);
-    obj.velocity = new THREE.Vector3(0,0,0);
-    obj.age = 0.0;
-
-    obj.applyForce = function(source){
-        this.acceleration.add(source);
-    };
-    obj.updateSpeed = function(){
-        this.velocity.add(this.acceleration);
-        this.position.add(this.velocity);
-        this.acceleration.multiplyScalar(0);
-
-        
-        if(this.position.y < 0 && this.velocity.y < 0 ){
-        this.velocity.y *= -0.8;
-        this.velocity.y += gravity.y; // Ich verliere eine Iteration Gravity, sollte nicht so sein!
-        this.position.y = 0;
-        };
-
-        if(Math.abs(this.velocity.y) < 0.01 && this.position.y < 0.1){
-        this.velocity.y = 0;
-        this.position.y = 0;
-        };
-        
-
-    };
-
-    obj.changeMat = function(){
-        if(this.velocity.y <= 0.1){
-        this.children[0].children[0].material.emissive = new THREE.Color(0x0000ff);
-        };
-
-        if(this.velocity.y > 0.1){
-        this.children[0].children[0].material.emissive = new THREE.Color(0xff0000);
-        };
-    };
-
-    obj.updateAge = function(t){
-        if(!this.birth) {this.birth = Math.floor(0.001*t)};
-        this.age = Math.floor(0.001*t)-this.birth;
-    };
-};
