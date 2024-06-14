97a91de7f9a1dee37afb1cfbed827e77d3f3bf0c
Loeffeldieb <jollegaming@gmx.de>
2024-06-14 13:03:03 +0200

    Update JS_Extrude.js

--- JS_Extrude.js
+++ JS_Extrude.js
@@ -9,10 +9,11 @@ import { XRButton } from 'three/addons/webxr/XRButton.js';
                                             Three.js Setup   Initialisierung
 ***************************************************************************************************************************/
 
-const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );  //  <-----
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: false
+ } );  //  <-----
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.xr.enabled = true;                                                    //  <-----
+renderer.xr.enabled = false;                                                    //  <-----
 
 const scene = new THREE.Scene();
 
@@ -22,8 +23,11 @@ camera.position.set(0, 3, 10);
 camera.lookAt(0,0,0);
 camera.updateProjectionMatrix();
 
-const light = new THREE.HemisphereLight(new THREE.Color(0xffffff), new THREE.Color(0x9999ff), 1);
-scene.add( light );
+const light = new THREE.DirectionalLight(0xFFFFFF, 3);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
+scene.add(light);
+scene.add(light.target);
 
 document.body.appendChild( renderer.domElement );
 
@@ -43,14 +47,35 @@ const sessionInit = {
         ]
 };
 
-document.body.appendChild(XRButton.createButton(renderer, sessionInit));      // <-----
+//document.body.appendChild(XRButton.createButton(renderer, sessionInit));      // <-----
 
 
-//Variablen für die Hand Erkennung und Controller
+
+
+/**************************************************************************************************************************
+                                            Zusätzliche Setups
+***************************************************************************************************************************/
+
+
+// Raycaser für die Maus
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+const pointA = {
+  position: new THREE.Vector3(),
+  isSet: 0
+};
+const pointB = {
+  position: new THREE.Vector3(),
+  isSet: 0
+};
+
+
+
+
+// Variablen für die Hand Erkennung und Controller
 let controller;
 
-
-//Standard Controller
+// Standard Controller
 controller = renderer.xr.getController( 0 );
 scene.add( controller );
 
@@ -64,18 +89,39 @@ scene.add( controller );
 
 function initialPromise(){
     return new Promise( resolve => {
+
+      // Erstelle die Textur für die Plane
+      const loader = new THREE.TextureLoader();
+      const texture = loader.load('bg.png');
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.magFilter = THREE.NearestFilter;
+      texture.colorSpace = THREE.SRGBColorSpace;
+      const repeats = 10 / 2;
+      texture.repeat.set(repeats, repeats);
+
+        const planeGeo = new THREE.PlaneGeometry(10,10,10,10);
+        planeGeo.rotateX(-Math.PI / 2);
         const plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(10,10, 10, 10),
-            new THREE.MeshBasicMaterial({
-                color: 0x272727,
-                wireframe: true,
+            planeGeo,
+            new THREE.MeshNormalMaterial({
+                //color: 0x272727,
+                //wireframe: true,
+                //map: texture,
                 side: THREE.DoubleSide
             })
         );
-        
-        plane.rotateX(Math.PI*-0.5);
-        plane.position.set(0,0,0);
         scene.add(plane);
+
+        const box = new THREE.Mesh(
+          new THREE.BoxGeometry(1,1,1),
+          new THREE.MeshNormalMaterial({
+            color: 0xFF0000
+          })
+        );
+        box.position.set(0,0.5,0);
+        scene.add(box);
+
         resolve(`Ground Plane loaded`);
     });
 };
@@ -103,9 +149,9 @@ Promise.all([p]).then( (resolve) => {
 
 function animate(timestamp, frame) {
 
-    //Animationen
+    // Animationen
     
-    //Rendert Frame
+    // Rendert Frame
     renderer.render( scene, camera );
 };
 
@@ -115,19 +161,21 @@ function animate(timestamp, frame) {
                                            Körper
 ***************************************************************************************************************************/
 
-//Zeichnet Linie ausgehend vom Controller
+// Zeichnet Linie ausgehend vom Controller
+/*
 const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
 const line = new THREE.Line( geometry );
 line.name = 'line';
 line.scale.z = 5;
 controller.add( line.clone() );
+*/
 
 //obj.position.set(0,0,-Math.random()*5).applyMatrix4( controller.matrixWorld );
 //obj.quaternion.setFromRotationMatrix( controller.matrixWorld ); //Drehung im Moment Deaktiviert
 
 
 /**************************************************************************************************************************
-                                           Funktions
+                                           Funktionen
 ***************************************************************************************************************************/
 
 function loadGLTF(url){
@@ -142,11 +190,52 @@ function loadGLTF(url){
 
 
 
-//Select Event 
+// Select Event 
 controller.addEventListener( 'select', onSelect );
-
 function onSelect(){
-    
+   
+};
+
+
+// Mouse Move Event
+window.addEventListener( 'pointermove', onPointerMove );
+function onPointerMove( event ) {
+  
+};
+
+// Mouse click Event
+window.addEventListener( 'click', onClick );
+function onClick( event ) {
+
+  //Get Mouse Position (Normalized)
+  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1; 
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  //Cast Ray and get Objects
+  raycaster.setFromCamera( pointer, camera );
+  const intersects = raycaster.intersectObjects( scene.children );
+  const firstHit = intersects[0];
+
+
+  //Lade Entchen an der Pointer stelle
+  if(intersects.length > 0){
+
+      const pointMarker = new THREE.Mesh(
+        new THREE.SphereGeometry(.05),
+        new THREE.MeshBasicMaterial({
+          color: (pointA.isSet==false)?0xFF0000:0x0000FF
+        })
+      );
+      pointMarker.lookAt( firstHit.face.normal );
+      pointMarker.position.copy( firstHit.point );
+      scene.add(pointMarker);
+
+      if(pointA.isSet && !pointB.isSet){pointB.isSet |= 1};
+      if(!pointA.isSet){pointA.isSet |= 1};
+      console.table(pointA.isSet, pointB.isSet);
+      //obj.rotateX(Math.PI / 2);
+      
+  };
+  
 };
 
 
@@ -161,7 +250,7 @@ addEventListener("resize", (event) => {
         camera.updateProjectionMatrix()
         renderer.setSize( window.innerWidth, window.innerHeight );
     };
-})
+});
 
 
 
