a9cea1f95a22b65597036bde4e737414f7ec1d5a
Loeffeldieb <jollegaming@gmx.de>
2024-07-08 15:52:38 +0200

    Update interactiveObjects.js

--- WebXr/interactiveObjects.js
+++ WebXr/interactiveObjects.js
@@ -56,16 +56,53 @@ class interactiveObjects{
 
     preloadMenuObjects( callback ){
         //Array für die Menu Objekte
-        this.menuObjects = new Array( 2 );
+        this.menuObjects = [];
 
         Promise.all([
             this._loadGLTF( './gltf/bunny/scene.gltf' ),
-            this._loadGLTF( './gltf/ente/Duck.gltf' )
+            this._loadGLTF( './gltf/ente/Duck.gltf' ),
+            this._loadGLTF( './gltf/tisch/scene.gltf' ),
+            this._loadGLTF( './gltf/fahrrad/scene.gltf' ),
+            this._loadGLTF( './gltf/kiste/scene.gltf' ),
+            this._loadGLTF( './gltf/workbench/scene.gltf' ),
         ]).then( (result) => { 
+
             for(let i=0; i<result.length; i++){
+                //Normalisiere die Größe der Objekte
+                const bbox = new THREE.Box3().setFromObject( result[i].scene );
+                let size = new THREE.Vector3();
+                bbox.getSize( size );
+                const scaleVec = new THREE.Vector3(1,1,1).divide( size );
+                const scale = Math.min( scaleVec.x, Math.min( scaleVec.y, scaleVec.z ));
+                result[i].scene.scale.setScalar( scale );
+
+
+                //Fülle Position nachrichten weil unterschiedlicher Ursprung in den Modellen
+                //Objekte sollten vorher shcon ausgerichtet werden
+                //das hier ist scheiße
+                switch (i){
+                    case 0:
+                        console.log( result[i].scene );
+                        //result[i].scene.children[0].children[0].children[0].children[0].children[0].position.set( 0,-0.25,0 );
+                        break;
+                    case 1:
+                        result[i].scene.position.set( 0,-5,0 );
+                        break;
+                    case 2:
+                        result[i].scene.position.set( 0,0,0 );
+                        break;
+                    case 3:
+                        result[i].scene.position.set( 0,0,0 );
+                        break;
+                    case 4:
+                        result[i].scene.position.set( 0,0,0 );
+                        break;
+                    case 5:
+                        result[i].scene.position.set( 0,0,0 );
+                        break;
+                };
                 this.menuObjects[i] = result[i].scene;
             };
-            this.menuObjectsLoaded = true;
 
             //Callback
             callback();
