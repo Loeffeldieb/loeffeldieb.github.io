fd1c16d2d76a989e647b65069e9851ad72869249
Loeffeldieb <jollegaming@gmx.de>
2024-07-11 15:20:02 +0200

    Update main.js

--- WebXr/main.js
+++ WebXr/main.js
@@ -61,7 +61,17 @@ class Game{
     startLoop(){
         // setAnimationLoop() zwingend notwendig für XR Anwendungen
         this.renderer.setAnimationLoop( (timestamp, frame) => {
-            if( this.gui.menuVisible ){this.gui.alignMenu( this.env.camera )};
+            //Menu Spezifische Funktionen
+            if( this.gui.menuVisible ){
+                this.gui.alignMenu( this.env.camera );
+
+                //Durchlaufe Objekte  muss geändert werden wenn möglich
+                //sodass das Array nicht immer ganz durhclaufen wird
+                this.objHandler.menuObjects.forEach( e => {
+                    if( e.isHovered ){ e.rotateY( 180/Math.PI * 0.0001 ) }
+                });
+            };
+
             this.env.animateShader( timestamp, this.renderer.domElement.width, this.renderer.domElement.height );
             this._drawFrame();
         });
@@ -83,20 +93,32 @@ class Game{
         switch ( event.key ){
             case 'b':
             this.objHandler.buildModeActivated = !this.objHandler.buildModeActivated;
-            //if( menu.visible ){raycasterGroup.add( menu )}else{raycasterGroup.remove( menu )};
             console.log(`Bau Modus ist ${this.objHandler.buildModeActivated?'an':'aus'}`);
             break;
             case 'r':
             this.objHandler.buildModeActivated = false;
             this.objHandler.activeObjectIsPlaced = false;
-            this.env.scene.remove( this.objHandler.testObj );
+            this.objHandler.activeObject = null;
+            this.env.scene.remove( this.objHandler.activeObject );
+            //Hier lösche ale neu hinzugefügten Elemente
+            // for(){
+
+            // };
             this.env.scene.remove( this.gui.lineForRotation )
             console.log( "Delete" );
             break;
             case 'q':
                 this.gui.menuVisible = !this.gui.menuVisible;
                 if(this.gui.menuVisible) { this.env.raycasterGroup.add( this.gui.menuGroup ) }
-                else { this.env.raycasterGroup.remove( this.gui.menuGroup ) };
+                else {
+                    if( this.gui.activeElement !== null){
+                        this.gui.activeElement.children[1].isHovered = false;
+                        this.gui.activeElement = null;
+                    };
+                    this.gui.activeElement = null;
+                    this.objHandler.activeObject = null;
+                    this.env.raycasterGroup.remove( this.gui.menuGroup ) 
+                };
             break;
             default:
             break;
@@ -109,48 +131,103 @@ class Game{
         // Cast Ray through Mouse ( Origin, targetGroup )
         this.gui.updateRaycasterTarget( this.env.camera, this.env.raycasterGroup.children );
 
+
+        //Menu spezifisch
+        if( this.gui.menuVisible ){
+            //Neuer Ray auf nur auf das Menu
+            this.gui.updateRaycasterTarget( this.env.camera, this.gui.menuGroup.children );
+            //Hover Logic für das Menu
+            if( this.gui.firstHit ){
+                if( this.gui.activeElement !== this.gui.getGridCard( this.gui.firstHit.object ) ){
+                    //Setze alte flag auf flase
+                    if( this.gui.activeElement !== null){
+                        this.gui.activeElement.children[1].isHovered = false;
+                    };
+                    //Setze neue flag auf true
+                    this.gui.activeElement = this.gui.getGridCard( this.gui.firstHit.object );
+                    this.gui.activeElement.children[1].isHovered = true;
+                };
+            }else{
+                //Nichts getroffen Setze alles zurück
+                if( this.gui.activeElement !== null){
+                    this.gui.activeElement.children[1].isHovered = false;
+                };
+                this.gui.activeElement = null;
+            };
+        };
+
+
         if(this.gui.firstHit){
             // Transformiere die getroffene Fläche in den World Space
             this.gui.normalToWorldSpace();
             // Richte den Marker passend zur Fläche aus
             this.gui.alignMarker( this.gui.firstHit.point, this.gui.normalWorldSpace );
-            this.env.scene.add( this.gui.marker );                                          //<--- Brauch FLag sonst redundant
-        };//Ende IF Element getroffen
+            this.env.scene.add( this.gui.marker );    
+            
+            //Baumodus platzierung
+            if(this.objHandler.buildModeActivated && !this.objHandler.activeObjectIsPlaced){
+                //Richte das Test Objekt aus
+                this.objHandler.activeObject.position.copy( this.gui.firstHit.point );
+                this.objHandler.activeObject.quaternion.copy ( this.gui.quaternionForMarker );
+            };
 
-        if(this.objHandler.activeObjectIsPlaced){
+        };
+
+        
+        //Baumodus Drehung
+        if(this.objHandler.buildModeActivated && this.objHandler.activeObjectIsPlaced){
             //Brechne Schnittpunkt Ray mit Normal Plane - Speichere in Intersect
             this.gui.raycaster.ray.intersectPlane( this.gui.temporaryPlane, this.gui.intersect );
             //Richte den Marker basierend of der neuen Ebene aus
             this.gui.alignMarker( this.gui.intersect, this.gui.temporaryPlane.normal );
             //Erstelle Hilfslinie für die Rotation
             this.env.scene.remove( this.gui.lineForRotation );
-            this.gui.createLine( this.objHandler.testObj.position, this.gui.intersect );
+            this.gui.createLine( this.objHandler.activeObject.position, this.gui.intersect );
             this.env.scene.add( this.gui.lineForRotation );
-            //Richte das Test Objekt aus
-            this.objHandler.testObj.up = this.gui.temporaryPlane.normal;
-            this.objHandler.testObj.lookAt( this.gui.intersect );
-        };
+            //Richte das active Objekt aus
+            this.objHandler.activeObject.up = this.gui.temporaryPlane.normal;
+            this.objHandler.activeObject.lookAt( this.gui.intersect );
+        }
     };
 
     //Funktion für das Click Event der Mouse
     onPointerClick( event ){
 
+        //Reset State to blank after objact is placed and rotated
         if( this.objHandler.buildModeActivated && this.objHandler.activeObjectIsPlaced ){
             this.objHandler.buildModeActivated = false;
             this.objHandler.activeObjectIsPlaced = false;
             this.env.scene.remove( this.gui.lineForRotation );
+            //this.objHandler.activeObject = null;  // würde sich aber noch im später bewegen lassen
         };
 
+
+        //Objekt wurde platziert
         if( this.gui.firstHit && this.objHandler.buildModeActivated ){
             //Erweitere Hit Normal in die Unendlichkeit zum abtasten
             this.gui.temporaryPlane.setFromNormalAndCoplanarPoint( this.gui.normalWorldSpace, this.gui.firstHit.point);
             
             // Setze Objekt basierend auf marker Position
-            this.objHandler.testObj.position.copy( this.gui.firstHit.point );
-            this.env.scene.add( this.objHandler.testObj );
             this.objHandler.activeObjectIsPlaced = true;
         };  
+
+        //Wähle hovered Element aus
+        if( this.gui.firstHit && this.gui.activeElement && this.gui.activeElement.children[1].isHovered ){
+            //Erstelle Kopie aus einem der Menu Objekte
+            this.objHandler.activeObject = this.gui.activeElement.children[1].clone();
+            this.objHandler.activeObject.scale.setScalar(0.3);
+            this.objHandler.activeObject.children[0].position.set( 0,0.0,0 );
+            //Schließe Menu und setze relevante Flags zurück
+            this.gui.menuVisible = false;
+            this.env.raycasterGroup.remove( this.gui.menuGroup );
+            this.gui.activeElement.children[1].isHovered = false;
+            this.gui.activeElement = null;
+            //Flag für den BuildModus
+            this.objHandler.buildModeActivated = true;
+            this.env.scene.add( this.objHandler.activeObject );
+        };
     };    
+
     
 };//Ende Game Klasse
 
