ea16f8d7201336cf710f92ad5cdde3c3f0b0ecaa
Loeffeldieb <jollegaming@gmx.de>
2024-07-16 13:20:38 +0200

    Update main.js

--- WebXr/main.js
+++ WebXr/main.js
@@ -51,9 +51,8 @@ class Game{
                 this.env.ctx.fillText(`Connected?: ${this.env.isConnected}`,10,20);
                 
                 if( this.env.controller.gamepad ){
-                    this.env.ctx.fillText(`B-Length: ${this.env.controller.gamepad.buttons.length}`,10,40);
                     for(let i=0; i<this.env.controller.gamepad.buttons.length; i++){
-                        this.env.ctx.fillText(`${i}: ${this.env.controller.gamepad.buttons[i].pressed},${this.env.controller.gamepad.buttons[i].touched},${this.env.controller.gamepad.buttons[i].value.toFixed(1)}`,10,60+20*i)
+                        this.env.ctx.fillText(`${i}: ${this.env.controller.gamepad.buttons[i].pressed},${this.env.controller.gamepad.buttons[i].touched},${this.env.controller.gamepad.buttons[i].value.toFixed(1)}`,10,40+20*i)
                     };
                 };
             
