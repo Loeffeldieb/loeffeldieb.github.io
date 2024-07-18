f4e5f4a73944665c0862878ea6f6a8804e75c500
Loeffeldieb <jollegaming@gmx.de>
2024-07-18 10:31:17 +0200

    Update shaders.js

--- WebXr/shaders.js
+++ WebXr/shaders.js
@@ -17,77 +17,46 @@ varying vec2 v_uv;
 uniform vec3 iResolution;
 uniform float iTime;
 
-#define MAX_STEPS 100
-#define MAX_DIST 100.0
-#define MIN_DIST 0.01
-
-float sdSphere(vec3 p, vec3 sp, float radius){
-    p -= sp;
-    return length(p) - radius;
+vec3 palette(float t)
+{
+    vec3 a = vec3(0.698,0.500, 0.898);
+    vec3 b = vec3(-0.542, 0.500, 0.248);
+    vec3 c = vec3(-0.722, 1.000, 1.000);
+    vec3 d = vec3(0.000, 0.333, 0.938);
+    return a + b*cos( 6.28318*(c*t+d) );
 }
 
-float plane(vec3 p, float pp){
-    float plane = p.y - pp;
-    return plane;
-}
-
-float drawScene(vec3 p){
-    float distanceToScene = 0.0;
-    float sphere = sdSphere(p, vec3(0.0,0.0,0.0), 1.0);
-    float plane = plane(p, -0.25);
-    distanceToScene = min(sphere, plane);
-    return distanceToScene;
-}
-
-float raymarch(vec3 ro, vec3 rd){
-    float dist = 0.0;
-    for(int i=0;i<MAX_STEPS;i++){
-        vec3 p = ro + rd * dist;
-        float distanceFromPoint = drawScene(p);
-        dist += distanceFromPoint;
-        if(dist<MIN_DIST || distanceFromPoint>MAX_DIST)break;
-    };
-    return dist;
-}
-
-vec3 calcNormal(vec3 p){
-    vec2 e = vec2(0.001, 0.0);
-    float x = drawScene(p+e.xyy)-drawScene(p-e.xyy);
-    float y = drawScene(p+e.yxy)-drawScene(p-e.yxy);
-    float z = drawScene(p+e.yyx)-drawScene(p-e.yyx);
-    vec3 n = vec3(x,y,z);
-    return normalize(n);
-}
-
-
-float diffuseLight(vec3 p, vec3 lightPos){
-    vec3 lightDir = normalize(p - lightPos);
-    float diff = dot(calcNormal(p), -lightDir);
-    diff = clamp(0.0,1.0,diff);
-    return diff;
-}
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     //Normalize
-    vec2 uv = fragCoord / iResolution.xy - 0.5;
+    vec2 uv = fragCoord / iResolution.xy * 2.0 - 1.0;
     uv.x *= iResolution.x / iResolution.y;
-    
-    //Variables
+
+    uv = v_uv;
+     
+    //Variablen
     vec3 color = vec3(1.0);
-    vec3 ro = vec3(0.0,1.0,-5.0);
-    vec3 rd = normalize(vec3(uv, 1.0));
     
-    //Raymarch
-    float d = raymarch(ro, rd);
-    vec3 p = ro + rd * d;
+    vec3 finalColor = vec3(0.0);
+    vec2 uv0 = v_uv - 0.5;
     
-    vec3 lightPos = vec3(2.0*sin(iTime),5.0,2.0*cos(iTime));
-    color *= diffuseLight(p, lightPos);
+    for(float i = 0.0; i<2.0; i++){
+        uv = fract(uv*2.0) - 0.5;
+
+        float d = length(uv) - 0.9;
+        color = palette(length(uv0) +iTime);
+
+
+        d = sin(d*8.+ iTime*3.0) / 8.;
+        d = abs(d);
+        d = 0.02 / d;
+
+        finalColor += color * d;
+    };
     
-    
-    //Output
-    fragColor = vec4(color, 1.0);
+    //output
+    fragColor = vec4(finalColor, 1.0);
 }
  
 void main() {
@@ -111,6 +80,7 @@ const _line_fs =
 `
 uniform vec3 origin;
 uniform vec3 color;
+uniform float time;
 varying vec3 vPos;
 float limitDistance = 0.5;
 
