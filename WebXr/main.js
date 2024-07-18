df715a5bc327ec5756280fc2c32cd0d427c8915a
Loeffeldieb <jollegaming@gmx.de>
2024-07-18 12:50:55 +0200

    Update main.js

--- WebXr/main.js
+++ WebXr/main.js
@@ -134,11 +134,11 @@ class Game{
                 this.objHandler.activeObject = null;
                 this.env.scene.remove( this.objHandler.activeObject );
                 //Hier lösche ale neu hinzugefügten Elemente
-                for(let i=this.env.raaycaster.children.length-1; i>=0; i--){
+                for(let i=this.env.raycaster.children.length-1; i>=0; i--){
                     if( this.env.raycaster.children[i]['name'] == 'placedObject' ){
                         //this.env.scene.remove( this.env.scene.children[i] );
                         this.env.raycasterGroup.remove( this.env.raycaster.children[i] );               //<----- Hier Testen
-                        this.objHandler.placedObjects.splice(i,1);
+                        //this.objHandler.placedObjects.splice(i,1);
                     };
                 };
                 this.env.scene.remove( this.gui.lineForRotation )
