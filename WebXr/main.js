4b8ccb94129874272cd10ad38a4cf637a2d0e21b
Loeffeldieb <jollegaming@gmx.de>
2024-07-22 11:49:24 +0200

    Update main.js

--- WebXr/main.js
+++ WebXr/main.js
@@ -85,8 +85,8 @@ class Game{
     };
 
     _drawFrame(){
-        //this.env.composer.render(); //Post Processing
-        this.env.renderer.render( this.env.scene, this.env.camera );
+        this.env.composer.render(); //Post Processing
+        //this.env.renderer.render( this.env.scene, this.env.camera );
     };
 
     manageControllerButtonEvents(){
